143aae032329c010fe0599dad161a0102cd2dcf8
ROZ <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2018-01-25 13:39:22 +0900

    open-nekonium-poolに変更

--- build/env.sh
+++ build/env.sh
@@ -11,10 +11,10 @@ fi
 workspace="$PWD/build/_workspace"
 root="$PWD"
 ethdir="$workspace/src/github.com/sammy007"
-if [ ! -L "$ethdir/open-ethereum-pool" ]; then
+if [ ! -L "$ethdir/open-nekonium-pool" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
-    ln -s ../../../../../. open-ethereum-pool
+    ln -s ../../../../../. open-nekonium-pool
     cd "$root"
 fi
 
@@ -25,8 +25,8 @@ GOBIN="$PWD/build/bin"
 export GOPATH GOBIN
 
 # Run the command inside the workspace.
-cd "$ethdir/open-ethereum-pool"
-PWD="$ethdir/open-ethereum-pool"
+cd "$ethdir/open-nekonium-pool"
+PWD="$ethdir/open-nekonium-pool"
 
 # Launch the arguments with the configured environment.
 exec "$@"
