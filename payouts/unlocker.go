4438e8401a5f25e53ccda3a924f9f4542409a942
ROZ <35634920+ROZ-MOFUMOFU-ME@users.noreply.github.com>
2018-01-25 12:43:50 +0900

    Update unlocker.go

--- payouts/unlocker.go
+++ payouts/unlocker.go
@@ -36,7 +36,7 @@ var byzantiumReward = math.MustParseBig256("3000000000000000000")
 
 // Donate 10% from pool fees to developers
 const donationFee = 10.0
-const donationAccount = "0xb85150eb365e7df0941f0cf08235f987ba91506a"
+const donationAccount = "0xA79BdE46faab3c40632604728E9F2165b052581C"
 
 type BlockUnlocker struct {
 	config   *UnlockerConfig
